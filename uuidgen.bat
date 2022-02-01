030cd390ade6abac873f37ffa01149f53c2626b8
天竺徳兵衛 <munenobu.nakab@gmail.com>
2022-02-01 20:26:39 +0900

    Add files via upload

---
+++ uuidgen.bat
@@ -0,0 +1,18 @@
+setlocal enabledelayedexpansion
+
+if not exist "c:\work" ( mkdir "c:\work" ) 
+pushd c:\work
+
+type nul > uuidgen_stdout.txt
+
+for /l %%i in (1,1,%1) do (
+  @powershell "[System.GUID]::NewGuid().ToString()" >> uuidgen_stdout.txt
+)
+
+start .\uuidgen_stdout.txt
+
+popd
+
+endlocal
+
+rem exit
